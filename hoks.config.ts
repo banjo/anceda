c5b872855bc8145dbb0aa19d7100e7c166096fdd
Anton Ödman <anton.odman@gmail.com>
2025-02-03 13:30:10 +0100

    chore: add hoks for commit validation

---
+++ hoks.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "hoks";
+
+export default defineConfig({
+    installOnLockChange: true,
+    branchName: {
+        pattern: "^(feature/.+|bugfix/.+|hotfix/.+|release/.+|main|develop)$",
+        message: "Branch name must start with 'feature/', 'bugfix/', 'hotfix/', 'release/'",
+    },
+    enforceConventionalCommits: true,
+});
+
