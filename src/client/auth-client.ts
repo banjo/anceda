280f42b1c50d9849059dc6f2e8728a9de906a95a
Anton Ödman <anton.odman@gmail.com>
2025-02-06 15:56:25 +0100

    feat: add organization schema

--- src/client/auth-client.ts
+++ src/client/auth-client.ts
@@ -1,8 +1,10 @@
 import { Env } from "@/utils/env";
 import { createAuthClient } from "better-auth/react";
+import { organizationClient } from "better-auth/client/plugins";
 
 const env = Env.client();
 
 export const authClient = createAuthClient({
     baseURL: env.VITE_SERVER_URL,
+    plugins: [organizationClient()],
 });
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -2,9 +2,11 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import { Config } from "@/config";
+import { organization } from "better-auth/plugins";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
+    plugins: [organization()],
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
