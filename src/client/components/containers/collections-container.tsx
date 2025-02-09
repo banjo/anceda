87a7fe1738708ba5b294a49befd3eff26b72579e
David <david.markus.olsson@gmail.com>
2025-02-09 21:24:17 +0100

    chore: added date and sort

--- src/client/components/containers/collections-container.tsx
+++ src/client/components/containers/collections-container.tsx
@@ -6,18 +6,30 @@ export const CollectionsContainer = () => {
         {
             id: "123",
             name: "Alliansloppet 2023",
-            numberOfImages: 200,
+            numberOfImages: 70,
+            date: new Date("2023-08-13"),
         },
         {
             id: "456",
             name: "Alliansloppet 2024 ",
-            numberOfImages: 175,
+            numberOfImages: 100,
+            date: new Date("2024-08-20"),
+        },
+        {
+            id: "789",
+            name: "Alliansloppet 2022 ",
+            numberOfImages: 85,
+            date: new Date("2022-08-19"),
         },
     ];
 
+    const collectionsByDate = collections.sort(
+        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+    );
+
     return (
         <div className="container mx-auto py-10">
-            <DataTable columns={columns} data={collections} />
+            <DataTable columns={columns} data={collectionsByDate} />
         </div>
     );
 };
