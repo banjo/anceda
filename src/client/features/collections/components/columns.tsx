f914b99a9b810348c6bc17a78b58fb8d5f73bdf8
David <david.markus.olsson@gmail.com>
2025-02-09 22:01:24 +0100

    feat: added dropdown to collections

--- src/client/features/collections/components/columns.tsx
+++ src/client/features/collections/components/columns.tsx
@@ -1,4 +1,15 @@
 import { ColumnDef } from "@tanstack/react-table";
+import { Ghost, MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/client/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/client/components/ui/dropdown-menu";
 
 export type Collection = {
     id: string;
@@ -26,6 +37,33 @@ export const columns: ColumnDef<Collection>[] = [
         cell: ({ getValue }) => {
             const date = getValue() as Date;
             return date.toLocaleDateString("en-GB");
+        },
+    },
+    {
+        id: "action",
+        cell: ({ row }) => {
+            const collection = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => navigator.clipboard.writeText(collection.id)}
+                        >
+                            Copy id
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View collection</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            );
         },
     },
 ];
