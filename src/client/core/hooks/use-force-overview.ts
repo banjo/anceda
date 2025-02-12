858ba1f7983f250696d77350e733ddb8c2277cd5
Anton Ödman <anton.odman@gmail.com>
2025-02-12 19:19:41 +0100

    fix: only redirect on specific url

--- src/client/core/hooks/use-force-overview.ts
+++ src/client/core/hooks/use-force-overview.ts
@@ -1,13 +1,14 @@
 import { useAuth } from "@/client/core/providers/auth-provider";
-import { useNavigate } from "@tanstack/react-router";
+import { useLocation, useNavigate } from "@tanstack/react-router";
 import { useEffect } from "react";
 
 export const useForceOverview = () => {
     const auth = useAuth();
     const navigate = useNavigate();
+    const { pathname } = useLocation();
 
     useEffect(() => {
-        if (!auth.isPending && auth.isAuthenticated) {
+        if (!auth.isPending && auth.isAuthenticated && pathname === "/dashboard") {
             navigate({ to: "/dashboard/overview" });
         }
     }, [auth, navigate]);
