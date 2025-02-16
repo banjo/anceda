a590127b53b43fd15b9adb180859cd9d218efa91
Anton Ödman <anton.odman@gmail.com>
2025-02-16 21:12:27 +0100

    chore: better type for organization

--- src/server/core/models/organization.ts
+++ src/server/core/models/organization.ts
@@ -1,6 +1,7 @@
+import { invariant } from "@banjoanton/utils";
 import { Organization as DbOrganization, OrganizationType } from "@prisma/client";
 
-export type Organization = {
+type OrganizationBase = {
     id: string;
     name: string;
     slug?: string;
@@ -9,15 +10,50 @@ export type Organization = {
     orgnizationType: OrganizationType;
 };
 
+export type PrimaryOrganization = OrganizationBase & {
+    orgnizationType: "PRIMARY";
+    invitedOrganizations: string[]; // Organization IDs
+};
+
+export type SecondaryOrganization = OrganizationBase & {
+    orgnizationType: "SECONDARY";
+    primaryOrganization: string; // Organization ID
+};
+
+export type Organization = PrimaryOrganization | SecondaryOrganization;
+
+const primary = (org: PrimaryOrganization) => org;
+const secondary = (org: SecondaryOrganization) => org;
 const from = (org: Organization) => org;
 
-const fromDb = (dbOrg: DbOrganization): Organization => ({
-    id: dbOrg.id,
-    name: dbOrg.name,
-    slug: dbOrg.slug ?? undefined,
-    logo: dbOrg.logo ?? undefined,
-    createdAt: dbOrg.createdAt,
-    orgnizationType: dbOrg.type,
-});
+const fromDb = (dbOrg: DbOrganization): Organization => {
+    const base: OrganizationBase = {
+        id: dbOrg.id,
+        name: dbOrg.name,
+        slug: dbOrg.slug ?? undefined,
+        logo: dbOrg.logo ?? undefined,
+        createdAt: dbOrg.createdAt,
+        orgnizationType: dbOrg.type,
+    };
 
-export const Organization = { from, fromDb };
+    if (dbOrg.type === "PRIMARY") {
+        return {
+            ...base,
+            orgnizationType: "PRIMARY",
+            invitedOrganizations: [], // TODO: get invited organizations
+        };
+    }
+
+    invariant(
+        dbOrg.primaryOrganizationId,
+        "Secondary organization must have a primary organization"
+    );
+
+    return {
+        ...base,
+        orgnizationType: "SECONDARY",
+        primaryOrganization: dbOrg.primaryOrganizationId,
+    };
+};
+
+export const Organization = { from, fromDb, primary, secondary };
